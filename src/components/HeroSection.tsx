31d473699c7db054d5c41d8f941ff79b3911c56c
Kundan0097 <kundan00092@gmail.com>
2025-05-08 14:57:00 +0530

    one change in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -64,7 +64,7 @@ const HeroSection = () => {
       </div>
 
       
-        <div className='absolute top-10 right-10'>
+        <div className='absolute top-5 right-10'>
         {
          message ? <p className='bg-green-700 text-white px-4 py-2 rounded'>{message}</p> : ""
         }
